98db07abd5725d956d92caad0ac3f8a02cf4cab1
Ivan Ablamskyi <acrist@me.com>
2013-03-22 11:34:59 +0200

    Initial Commit

---
+++ Themes/IADocument.h
@@ -0,0 +1,13 @@
+//
+//  IADocument.h
+//  Themes
+//
+//  Created by Ivan Ablamskyi on 22.03.13.
+//  Copyright (c) 2013 Coppertino Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface IADocument : NSDocument
+
+@end
